3f3c5868e9794f64defb2a93c9efee2b1baf53f3
Vladimir Ilyin <oivt@ya.ru>
2023-01-27 18:38:30 +0300

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/WebFragment.java
@@ -0,0 +1,52 @@
+package com.example.myapplication;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+
+public class WebFragment extends Fragment {
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup parent, @Nullable Bundle savedInstanceState) {
+        // Inflate the xml file for the fragment
+        return inflater.inflate(R.layout.fragment_web, parent, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        WebView webView = view.findViewById(R.id.webView);
+        webView.getSettings().setJavaScriptEnabled(true);
+
+
+        final Activity activity = getActivity();
+
+         webView.setWebViewClient(new WebViewClient() {
+            public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+                Toast.makeText(activity, description, Toast.LENGTH_SHORT).show();
+            }
+        });
+        //webView.loadUrl("http://...");
+    }
+
+}
